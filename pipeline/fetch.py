dfd3f5d45e5109e5a484e1f6d256f452816c90f9
tlayers21 <tlayers21@gmail.com>
2025-12-22 17:26:23 -0500

    getting started with day 5 (creating USDA client)

--- pipeline/fetch.py
+++ pipeline/fetch.py
@@ -1,7 +1,7 @@
 import requests, json
 from pathlib import Path
+from typing import List, Dict, Any
 from .utils import raw_data_path
-
 BASE_URL = "https://api.fas.usda.gov"
 
 class USDAClient:
@@ -10,7 +10,7 @@ class USDAClient:
         self.base_dir = base_dir
 
         
-    def _get(self, endpoint: str) -> any
+    def _get(self, endpoint: str) -> List[Dict[str, Any]]:
         url = BASE_URL + endpoint
         params = {"api_key": self.api_key}
 
@@ -21,3 +21,17 @@ class USDAClient:
             )
         
         return response.json()
+    
+    def save_raw_data(self, data, filename: str) -> Path:
+        raw_dir = self.base_dir/"data"/"raw"
+        raw_dir.mkdir(parents=True, exist_ok=True)
+
+        raw_path = raw_dir/filename
+        with open(raw_path, "w") as f:
+            json.dump(data, f, indent=2)
+        
+        return raw_path
+    
+    def esr_exports_all(self, commodity_code: str, market_year: str) -> List[Dict[str, Any]]:
+        endpoint = f"/api/esr/exports/commodityCode/{commodity_code}/allCountries/marketYear/{market_year}"
+        return self._get(endpoint)
