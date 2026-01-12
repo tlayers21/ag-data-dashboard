a3a4cc78812870533d75940b3cbc6e6e71f79337
tlayers21 <tlayers21@gmail.com>
2026-01-12 16:27:27 -0500

    all done with utils

--- pipeline/utils.py
+++ pipeline/utils.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 
+# Root directroy for project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# For raw ESR and PSD data
 def fas_data_path(filename: str) -> Path:
     return BASE_DIR / "data" / "raw" / "fas" / filename
 
+# For raw inspections data
 def inspections_data_path(filename: str) -> Path:
     return BASE_DIR / "data" / "raw" / "inspections" / filename
 
+# For cleaned CSVs
 def clean_data_path(filename: str) -> Path:
     return BASE_DIR / "data" / "clean" / filename
