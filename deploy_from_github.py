f3013b5ebe1b0ecf5835114e9530773b61cfcf67
tlayers21 <tlayers21@gmail.com>
2026-03-13 15:03:35 -0400

    Updated Prefect Deployment File

--- deploy_from_github.py
+++ deploy_from_github.py
@@ -1,9 +1,18 @@
 from prefect import flow
 from prefect.runner.storage import GitRepository
+from prefect.client.schemas.schedules import CronSchedule
 
 source = GitRepository(
     url="https://github.com/tlayers21/ag-data-dashboard"
 )
+
+schedules = [
+    CronSchedule(cron="15 11 * * MON", timezone="America/New_York"),  
+    CronSchedule(cron="00 12 * * MON", timezone="America/New_York"), 
+    CronSchedule(cron="00 09 * * THU", timezone="America/New_York"),  
+    CronSchedule(cron="00 12 * * THU", timezone="America/New_York"), 
+    CronSchedule(cron="00 12 * * TUE,WED,FRI,SAT,SUN", timezone="America/New_York") 
+]
 
 flow.from_source(
     source=source,
@@ -11,5 +20,5 @@ flow.from_source(
 ).deploy(
     name="ag-data-dashboard",
     work_pool_name="agdatadashboard-pool",
+    schedules=schedules
 )
-
